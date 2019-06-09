03d94ca1c56b8b5b88e310e711b91bdb4514f565
Nathan <nathan.alard@absa.co.za>
2019-06-09 11:20:09 +0200

    minor changes

--- GuessTheNumber/GuessMyNumber.cpp
+++ GuessTheNumber/GuessMyNumber.cpp
@@ -6,18 +6,16 @@ void PromptUserToGuess();
 string NewGuess();
 bool GuessedRight(int newGuess,int randomNumber);
 void YouGuessedRight(int guessTryCount, int randomNumber);
+string WelcomeUser();
 
 int main() {
     string userName = "";
-    cout<< "Welcome to Guess the number!" << endl << "Whats your name? ";
-    cin >> userName;
-    cout << endl << endl;
-    cout << "Hey " << userName << " Lets start guessing...." << endl << endl;
+    userName = WelcomeUser();
     bool continueGuessing = true;
-    int randomNumber = rand() % 100;
+    int randomNumber;
     
     while(continueGuessing) {
-        randomNumber = rand() % 100;
+        randomNumber = rand() % 10;
         PromptUserToGuess();
         int guessTryCount = 1;
         string guessOrGuessesForMessage;
@@ -29,25 +27,36 @@ int main() {
             int newGuessNumber = stoi(newGuess);
             if(GuessedRight(newGuessNumber, randomNumber)) {
                 cout << "Welldone " << userName << ", You guessed " << randomNumber;
-                cout << " in " << guessTryCount << guessOrGuessesForMessage;
+                cout << " in " << guessTryCount << " " << guessOrGuessesForMessage << endl;
                 break;
             } else {
                 string tooLowTooHighMessage;
                 if(newGuessNumber < randomNumber) tooLowTooHighMessage = "too low"; 
                 else tooLowTooHighMessage = "too high";
-                cout << "Oops you've guessed " << tooLowTooHighMessage << ", you have " << guessTryCount << " ";
-                cout << guessOrGuessesForMessage << " left." << endl;
-                guessTryCount++;
+                cout << "Oops you've guessed " << tooLowTooHighMessage << ", you have " << 3-guessTryCount << " ";
+                cout << guessOrGuessesForMessage << " left." << endl << endl;
             }
+            guessTryCount++;
         }
-        cout << "Do you want to try again?" << endl;
+        cout << endl << "Do you want to try again?" << endl;
         cout << "Enter y to continue or anything to exit..." << endl;
-        cout << "*****************************************************" << endl;;
+        
         string userResponse;
         cin >> userResponse;
         if(userResponse != "y") continueGuessing = false;
+        cout << endl << "*****************************************************" << endl << endl;
     }
+    cout << "Thanks for playing " << userName << "!" << endl;
     return 0;
+}
+
+string WelcomeUser() {
+    string userName;
+    cout<< "Welcome to Guess the number!" << endl << "Whats your name? ";
+    cin >> userName;
+    cout << endl << endl;
+    cout << "Hey " << userName << " Lets start guessing...." << endl << endl;
+    return userName;
 }
 
 bool GuessedRight(int guess, int numberToGuess) {
@@ -57,12 +66,11 @@ bool GuessedRight(int guess, int numberToGuess) {
 }
 
 string NewGuess() {
-    PromptUserToGuess();
     string guess;
     cin >> guess;
     return guess;
 }
 
 void PromptUserToGuess() {
-    cout << "Enter your below: ie. 0 - 100..." << endl << "You got 3 guesses, good luck!" << endl << endl;
+    cout << "Enter your Guess below: ie. 0 - 10..." << endl << "You got 3 guesses, good luck!" << endl << endl;
 }
